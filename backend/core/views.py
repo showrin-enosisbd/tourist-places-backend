9e41ea852d7de3c16670e40a5586f7502de99f33
Showrin <showrin.barua@enosisbd.com>
2022-09-19 14:24:28 +0600

    :truck: [Struct] Group create and get place api under one url

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,43 +1,40 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from core.models import Place
 from core.serializers import PlaceSerializer
 
 
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def create_place(request, format=None):
-    try:
-        serializer = PlaceSerializer(data=request.data)
-
-        if serializer.is_valid():
-            new_place = serializer.save()
-
-            response = {
-                'message': 'The place has been created successfully',
-                'place': {
-                    'id': new_place.id,
-                    'name': new_place.name,
-                    'address': new_place.address,
-                    'rating': new_place.rating,
-                    'type': new_place.type,
-                    'picture': new_place.picture,
-                }
-            }
-
-            return Response(response, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    except Exception as err:
-        return Response(err.__str__(), status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticatedOrReadOnly])
 def place_list(request, format=None):
-    places = Place.objects.all()
-    serializer = PlaceSerializer(places, many=True)
+    if request.method == 'GET':
+        places = Place.objects.all()
+        serializer = PlaceSerializer(places, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-    return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        try:
+            serializer = PlaceSerializer(data=request.data)
+
+            if serializer.is_valid():
+                new_place = serializer.save()
+
+                response = {
+                    'message': 'The place has been created successfully',
+                    'place': {
+                        'id': new_place.id,
+                        'name': new_place.name,
+                        'address': new_place.address,
+                        'rating': new_place.rating,
+                        'type': new_place.type,
+                        'picture': new_place.picture,
+                    }
+                }
+                return Response(response, status=status.HTTP_201_CREATED)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as err:
+            return Response(err.__str__(), status=status.HTTP_400_BAD_REQUEST)
