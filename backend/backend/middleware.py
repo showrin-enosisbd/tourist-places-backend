fc3c3da415002d37b912273449dc9c8aa251220c
Showrin <showrin.barua@enosisbd.com>
2022-09-21 19:38:23 +0600

    :sparkles: [feat] Write cors middleware to add cors headers to response

---
+++ backend/backend/middleware.py
@@ -0,0 +1,12 @@
+from urllib import response
+
+
+def cors_middleware(get_response):
+    def middleware(request):
+        response = get_response(request)
+        response['Access-Control-Allow-Origin'] = '*'
+        response['Access-Control-Allow-Headers'] = '*'
+
+        return response
+
+    return middleware
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'backend.middleware.cors_middleware'
 ]
 
 ROOT_URLCONF = 'backend.urls'
