01eee573769c482ded1f6ba8cd73b97ba257ca63
Showrin <showrin.barua@enosisbd.com>
2022-09-22 00:59:24 +0600

    :sparkles: [feat] add id, email, username to the login api response

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -1,14 +1,13 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
-from rest_framework.authtoken import views
 from django.contrib import admin
 
-from account.views import register_user, logout_user, user_list
+from account.views import LoginUser, register_user, logout_user, user_list
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('^account/register/', register_user, name='register-user'),
-    url('^account/login/', views.obtain_auth_token),
+    url('^account/login/', LoginUser.as_view(), name='login-user'),
     url('^account/logout/', logout_user, name='logout-user'),
     url('^account/users/', user_list, name='user-list'),
 ]
--- backend/account/views.py
+++ backend/account/views.py
@@ -1,4 +1,5 @@
 from rest_framework.decorators import api_view
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework import status
@@ -8,6 +9,22 @@ from django.contrib.auth import logout
 from account.serializers import UserSerializer
 from account.models import User
 from helpers.pagination import get_paginted_result
+
+
+class LoginUser(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+
+        return Response({
+            'token': token.key,
+            'id': user.pk,
+            'email': user.email,
+            'username': user.username,
+        })
 
 
 @api_view(['POST'])
