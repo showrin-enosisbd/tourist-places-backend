a5b90bb4aeaddb722a4609b92923c78982aa705a
Showrin <showrin.barua@enosisbd.com>
2022-09-22 14:41:01 +0600

    :bug: [bug] Hide password from the api response

--- backend/account/serializers.py
+++ backend/account/serializers.py
@@ -11,7 +11,7 @@ class UserSerializer(serializers.Serializer):
     email = serializers.EmailField(label=_("Email"), allow_null=False)
     password = serializers.CharField(label=_("Password"),
                                      style={'input_type': 'password'},
-                                     trim_whitespace=False)
+                                     trim_whitespace=False, write_only=True)
     places = serializers.PrimaryKeyRelatedField(
         many=True, read_only=True)
     extra_kwargs = {"password": {"write_only": True}}
