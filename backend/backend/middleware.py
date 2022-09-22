e56bfbfb11b5da593c7d16ee820115c8382959a3
Showrin <showrin.barua@enosisbd.com>
2022-09-22 16:58:58 +0600

    :bug: [bug] Add allowance for all http methods to prevent cors

--- backend/backend/middleware.py
+++ backend/backend/middleware.py
@@ -1,11 +1,9 @@
-from urllib import response
-
-
 def cors_middleware(get_response):
     def middleware(request):
         response = get_response(request)
         response['Access-Control-Allow-Origin'] = '*'
         response['Access-Control-Allow-Headers'] = '*'
+        response['Access-Control-Allow-Methods'] = '*'
 
         return response
 
